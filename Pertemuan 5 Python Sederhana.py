7aa4e2649086203ca4908190c91bba357cd9f648
elsa-rahma-hidayani <114091331+elsa-rahma-hidayani@users.noreply.github.com>
2023-03-02 09:20:15 +0700

    Add files via upload

---
+++ Pertemuan 5 Python Sederhana.py
@@ -0,0 +1,23 @@
+import re
+import sys
+# Mendefinisikan variabel untuk menyimpan jumlah sks dan total nilai
+total_sks = 0
+total_nilai = 0
+
+# Meminta input jumlah mata kuliah yang diambil oleh mahasiswa
+jumlah_mk = int(input("Berapa jumlah mata kuliah yang Anda ambil: "))
+
+# Meminta input nilai dan sks masing-masing mata kuliah
+for i in range(jumlah_mk):
+  print("Mata kuliah ke-", i+1)
+  nilai = float (input type="number" min="0" max="4.0" ("Masukkan nilai mata kuliah: "))
+
+  sks = int(input("Masukkan jumlah sks mata kuliah: "))
+  total_nilai += nilai * sks
+  total_sks += sks
+
+# Menghitung nilai IPK
+ipk = total_nilai / total_sks
+
+# Menampilkan nilai IPK
+print("Nilai IPK Anda adalah", ipk)
